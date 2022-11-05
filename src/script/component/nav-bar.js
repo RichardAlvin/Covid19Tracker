9a6585fee214dbb12f8cc6f4f71bf3a6a16b372e
Richard Alvin <richardalvinpratama8@gmail.com>
2022-11-05 23:28:41 +0700

    add navbar component

--- src/script/component/nav-bar.js
+++ src/script/component/nav-bar.js
@@ -19,16 +19,97 @@ class NavBar extends HTMLElement {
           :host {
             display: block;
             width: 100%;
-            background-color: cornflowerblue;
-            color: white;
+            background-color: white;
+            color: black;
             box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+            overflow: hidden;
+          }
+          :host a{
+            float: left;
+            display: block;
+            color: black;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-size: 17px;
+          }
+          :host a:hover{
+            background-color: #ddd;
+            color: black;
+          }
+          :host a.active{
+            background-color: #04AA6D;
+            color: white;
+          }
+          :host .icon{
+            display:none;
           }
           h2 {
             padding: 16px;
           }
+
+          :host .searchContainer {
+            float: right;
+          }
+
+          :host input[type=text] {
+            padding: 6px;
+            margin-top: 8px;
+            font-size: 17px;
+            border: none;
+          }
+          
+          :host .searchContainer button {
+            float: right;
+            padding: 6px 10px;
+            margin-top: 8px;
+            margin-right: 16px;
+            background: #ddd;
+            font-size: 17px;
+            border: none;
+            cursor: pointer;
+          }
+          
+          :host .searchContainer button:hover {
+            background: #ccc;
+          }
+
+          .searchButton{
+            border-radius: 10px;
+            background-color: green;
+            color: white;
+            padding: 10px;
+            border: none;
+          }
+
+          @media screen and (max-width: 600px) {
+            :host .searchContainer {
+              float: none;
+            }
+            :host a, :host input[type=text], :host .searchContainer button {
+              float: none;
+              display: block;
+              text-align: left;
+              width: 100%;
+              margin: 0;
+              padding: 14px;
+            }
+            :host input[type=text] {
+              border: 1px solid #ccc;  
+            }
+          }
         </style>
         
-        <h2>Club Finder</h2>
+        <a href="#">RAP_Movie</a>
+        <a href="#" class="active">Home</a>
+        <a href="favorite">Favorite</a>
+
+        <div class="searchContainer">
+          <form action="/action_page.php">
+            <input type="text" placeholder="Find Name of Movie" name="search">
+            <button class="searchButton" type="submit">Search</button>
+          </form>
+        </div>
       `;
     }
   }
